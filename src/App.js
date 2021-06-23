586f5367aafc5d9797ad08cb3455f751f7b7735a
Daniel Harding <dan.harding@myemail.indwes.edu>
2021-06-23 15:28:22 -0400

    addition of files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import React from 'react';
 import './App.css';
-import "../src/slideshow/controls.css";
-import "../src/slideshow/controls.js"
 import Slideshow from './slideshow/controls.js';
 import RegistrationForm from './Login/components/registrationForm';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import LoginForm from './Login/components/loginForm';
 import SearchBar from './SearchBar/components/searchBar';
-import AddCredit from './credits/creditspage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+
         <SearchBar />
         <Slideshow />
       </header>
-
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/login"></Redirect>
-          </Route>
-          <Route path="/mycredits">
-            <AddCredit />
-          </Route>
-          <Route path="/login" component={LoginForm} />
-          <Route path="/register" component={RegistrationForm} />
-        </Switch>
-      </Router>
-
+      <div className="routes">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/login"></Redirect>
+            </Route>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegistrationForm} />
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 
---
+++ src/navbarPages/bucketlist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BucketList extends Component {
+    render() {
+        return (
+            <div>
+                <h2>My Bucket List</h2>
+            </div>
+        );
+    }
+}
+
+export default BucketList;
