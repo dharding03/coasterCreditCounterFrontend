4a300e98b0c92ce6bd0f7f15cc5a3788d02d5940
Daniel Harding <dan.harding@myemail.indwes.edu>
2021-06-24 20:36:58 -0400

    trying to get links to show up as buttons

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React from 'react';
 import './App.css';
 import RegistrationForm from './Login/components/registrationForm';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import LoginForm from './Login/components/loginForm';
 import SearchBar from './SearchBar/components/searchBar';
 import Carousel from './slideshow/controls.js';
 import NavBar from './navbar/navbar';
+import BucketList from './navbar/bucketlist';
+import LoginForm from './Login/components/loginForm';
+import Credits from './navbar/rollerCoasters';
+
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <header className="App-header">
-
-
         <Carousel />
+
         <SearchBar />
       </header>
       <div className="routes">
@@ -23,8 +26,11 @@ function App() {
             <Route exact path="/">
               <Redirect to="/login"></Redirect>
             </Route>
+            <Route path="/bucketlist" component={BucketList}/>
             <Route path="/login" component={LoginForm} />
             <Route path="/register" component={RegistrationForm} />
+            <Route path="/home" component={App}/>
+            <Route path="/rollercoasters" component={Credits}/>
           </Switch>
         </Router>
       </div>
