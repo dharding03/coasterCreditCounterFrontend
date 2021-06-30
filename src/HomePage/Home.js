eb1ab26fb583cf838843201ba3cf45ca997c21e0
normabunton <te_nj25@hotmail.com>
2021-06-30 19:06:35 -0400

    homepage video implementing to just the home page

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -12,9 +12,9 @@ function Home() {
     <div className="home">
       <h1 id="header">Welcome to Coaster Credit Counter</h1>
       <p>Log in to Search and Build a Bucket List of Your Favorite Roller Coasters</p>
-<<<<<<< HEAD
+
       <iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/bCHM41ZdTzQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-=======
+
       [<iframe width="560" height="315"
         src="https://www.youtube.com/embed/bCHM41ZdTzQ"
         title="YouTube video player"
@@ -36,7 +36,7 @@ function Home() {
         allowfullscreen></iframe>
 
       ]
->>>>>>> 339372550d3a1833aec1d45171d9bb63c98af029
+
     </div>
 
   );
