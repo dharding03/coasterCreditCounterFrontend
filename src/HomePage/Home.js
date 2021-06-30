6c29884eee3ce3b34e990cda4911eee85af8b5af
normabunton <te_nj25@hotmail.com>
2021-06-30 18:50:12 -0400

    home page styling for video

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -12,9 +12,9 @@ function Home() {
 
   return (
     <div className="home">
-      <h1>Welcome to Coaster Credit Counter</h1>
+      <h1 id="header">Welcome to Coaster Credit Counter</h1>
       <p>Log in to Search and Build a Bucket List of Your Favorite Roller Coasters</p>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/bCHM41ZdTzQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/bCHM41ZdTzQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 
   );
