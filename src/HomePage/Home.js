d5dc40300c163e8a04a1f9329671e65ae759e0b9
Daniel Harding <dan.harding@myemail.indwes.edu>
2021-07-01 15:10:21 -0400

    added a myAccount page, added the carousel to it, got rid of some unused imports

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -32,6 +32,13 @@ function Home() {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen></iframe>
 
+      <iframe width="560" height="315"
+        src="https://www.youtube.com/embed/lIYF8isb0ko"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+
       ]
     </div>
 
