200ca64b0e7a58df85e2342ea8a182a0b52871a2
Daniel Harding <dan.harding@myemail.indwes.edu>
2021-07-01 22:03:19 -0400

    updated a route in rollerCoasters.js

--- src/navbar/rollerCoasters.js
+++ src/navbar/rollerCoasters.js
@@ -30,7 +30,7 @@ function Credits() {
                     "Access-Control-Allow-Origin": "*",
                 }
             }
-            axios.post(ApiBaseUrl + 'coasters/credits', payload, config)
+            axios.post(ApiBaseUrl + '/coasters/credits', payload, config)
                 .then(function (response) {
                     if (response.status === 200) {
                         setState(prevState => ({
