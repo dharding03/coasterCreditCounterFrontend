94ef061dd5b922468f60eb45b2f1658da233f336
normabunton <te_nj25@hotmail.com>
2021-07-02 08:10:03 -0400

    background video not complete yet

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -5,7 +5,7 @@ function NavBar() {
         <div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary" />
 
-                <a href={"/home"} class="home">Coater Credit Counter</a><br />
+                <a href={"/home"} class="home">Coaster Credit Counter</a><br />
                 <a class="navbar-brand" href={"/login"}>Login</a>
                 <a href={"/bucketlist"} class="nav-link">Bucket List</a>
                 <a href={"/rollercoasters"} class="nav-link">Roller Coasters</a>
