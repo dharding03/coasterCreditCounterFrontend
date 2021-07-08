fbd26c2440873009a8c0d235ce277239b0835c5f
Daniel Harding <dan.harding@myemail.indwes.edu>
2021-07-08 13:08:30 -0400

    got DB data to display

--- src/DisplayDBData/displayCredits.js
+++ src/DisplayDBData/displayCredits.js
@@ -3,25 +3,30 @@ import React, { useEffect, useState } from 'react';
 function DisplayCredits() {
 
     const [credits, setCredits] = useState([])
+    console.log(credits)
+    useEffect(() => {
+        fetch("http://localhost:7080/coasters/credits")
+        .then(response => response.json())
+        .then(json => setCredits(json));
+    }, []);
 
-    // useEffect(() => {
-    //     fetch("http://localhost7080/coasters/credits")
-    //     .then(response => response.json())
-    //     .then(json => setCredits(json));
-    // }, []);
-
-    function display(rows) {
-        return rows.filter((row) => 
-        row.coaster.toLowerCase().indexOf(credits) > -1 ||
-        row.park.toLowerCase().indexOf(credits) > -1
-        )
-    }
+    // function display(rows) {
+    //     return rows.filter((row) => 
+    //     row.coaster.toLowerCase().indexOf(credits) > -1 ||
+    //     row.park.toLowerCase().indexOf(credits) > -1
+    //     )
+    // }
 
     return (
         <div>
-            <p>
-              {(display(credits))}  
+            
+              {credits.map((coaster, id ) => 
+            <p key= {id}>
+                 
+               {coaster.coaster}
+               {coaster.park}   
             </p>
+              )}  
         </div>
     )
 
